830d186a32e20533fbe7fe88b2d3e0726f84aff5
Jamie Bonner <j.bonner6992@gmail.com>
2025-07-31 13:41:21 +0100

    Creates unit testing for playlist class
    Merging this PR will create unit testing for the Playlist class

---
+++ test/flicks/playlist_test.rb
@@ -0,0 +1,42 @@
+require "minitest/autorun"
+require_relative "../../lib/flicks/playlist"
+require_relative "../../lib/flicks/movie"
+
+class PlaylistTest < Minitest::Test
+
+    def setup
+        @playlist = Playlist.new("Gonzo")
+
+        @movie_1 = Movie.new("Bond", 10)
+        @movie_2 = Movie.new("Die Hard", 9)
+    end
+
+
+    def test_has_no_movies_initially
+        assert_empty @playlist.movies
+    end
+    
+    def test_add_movies
+        
+        @playlist.add_movie(@movie_1)
+        @playlist.add_movie(@movie_2)
+
+        refute_empty @playlist.movies
+        assert_equal [@movie_1, @movie_2], @playlist.movies
+    end
+
+    def test_high_number_rolled_gives_movie_thumbs_up        
+        @playlist.add_movie(@movie_1)
+
+        @playlist.stub(:roll_die,6) do
+            @playlist.play()
+
+            assert_equal 11, @movie_1.rank
+        
+        end
+
+
+
+    end
+
+end
