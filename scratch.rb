11ed62019e0bb19d22712dc5681f7b74890c0848
JamieCronofy <jamie@cronofy.com>
2025-08-07 15:34:13 +0100

    Refactor playlist class to play multiple time

---
+++ scratch.rb
@@ -0,0 +1,7 @@
+# Long block method to iterate over the episodes
+1.upto(9) do |episode|
+    puts "star wards: Episode #{episode}"
+end
+
+# Shorthand block method to iterate over the episodes 
+1.upto(9) { |episode| puts "star wards: Episode #{episode}"}
